edb28317f36d9516962fd05fdf80965304202604
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-08-17 19:25:01 +0300

    Lesson 3 homework read

---
+++ src/components/post/Post.js
@@ -0,0 +1,19 @@
+export function Post({item, choosePost}){
+    const onClickPost = () => {
+        choosePost(item.id);
+    }
+    return (
+        <div >
+               <h3>{item.userId}. {item.id}. {item.title}</h3>
+            <div>
+                <button onClick={onClickPost}>details</button>
+
+            </div>
+            <div className={'posts'}>
+
+
+
+            </div>
+        </div>
+    )
+}
---
+++ src/components/posts/Posts.js
@@ -0,0 +1,39 @@
+import {useEffect, useState} from "react";
+import {Post} from "../post/Post";
+import {getPosts} from "../../service/posts.service";
+
+
+
+
+export  function Posts(item){
+
+    let [posts, setPosts] = useState( []);
+    let [post, setPost] = useState([null])
+
+
+    useEffect(() =>{
+        getPosts().then(value => setPosts([...value]));
+    },[]);
+
+
+    const choosePost = (id) =>{
+       let choosenPost = posts.find(value => value.id == id);
+       setPost(choosenPost);
+        console.log(choosenPost);
+    }
+    return (
+        <div className={'posts'}>
+            <div className={'post'}>
+                {
+                    posts.map(value => <Post item={value} key={value.id} choosePost={choosePost}/>)
+                }
+            </div>
+
+                {
+                   post && <div>{post.body}</div>
+                }
+
+        </div>
+    );
+}
+
