4846908de8a6a9af92f25597be72af89712872d9
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-09-01 23:36:04 +0300

    Test_react break

--- src/service/movieService.js
+++ src/service/movieService.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 const instance = axios.create({
     baseUrl: 'https://api.themoviedb.org/3/',
-    headers: {Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2Y4YmJiZWQxZTliMDY2NzdlZjZkNTdkNjVhYzJlOCIsInN1YiI6IjYxMmY2MTlkZmFjNTAyMDAyNmZkY2EyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WrL-NnkEwRCaGFDHqrPJARIvFRXR_Rq3uSxg6wnrQY0'}
+    headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2Y4YmJiZWQxZTliMDY2NzdlZjZkNTdkNjVhYzJlOCIsInN1YiI6IjYxMmY2MTlkZmFjNTAyMDAyNmZkY2EyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WrL-NnkEwRCaGFDHqrPJARIvFRXR_Rq3uSxg6wnrQY0'
+    }
 });
 const discoverMovie = async() => await instance.get('discover/movie');
 export {discoverMovie};
