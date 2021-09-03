cd23fcd136ca844597ec0fc0afc58485a2781dfb
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-09-03 23:27:10 +0300

    Test_react break

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import {useEffect} from "react";
 import {getMovies} from "./service/movieService";
 import {useSelector,useDispatch} from "react-redux";
-
+import {
+    BrowserRouter as Router,
+    Route,
+    Link, Switch,
+} from "react-router-dom";
 import {Header} from "./components/Header";
 import {MoviesListCard} from "./components/MoviesListCard";
 function App() {
@@ -15,10 +19,21 @@ function App() {
   // }, []);
 
   return (
+      <Router >
       <div>
-        <Header/>
-          <MoviesListCard/>
+          <Header/>
+
+          <Link to={'/'}>Back</Link>
+          <Link to={'/MoviesListCard'}>movie</Link>
+
       </div>
+          <Switch>
+          <Route exact path={'/MoviesListCard'} render={()=><div>home page</div>}/>
+
+          <Route exact path={'/'} render={()=><div>home page</div>}/>
+      </Switch>
+
+          </Router>
   );
 }
 
