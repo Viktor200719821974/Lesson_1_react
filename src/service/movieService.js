2c790e08c56c48edb6d1e3fa659aeb0f18fcd3f3
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-09-09 09:46:36 +0200

    test react break

--- src/service/movieService.js
+++ src/service/movieService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {apiBaseUrl, urlGeners} from "../constants/api";
+import {apiBaseUrl} from "../constants/api";
 const axiosMovies = axios.create(
     {
         baseURL: apiBaseUrl,
@@ -7,7 +7,5 @@ const axiosMovies = axios.create(
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2Y4YmJiZWQxZTliMDY2NzdlZjZkNTdkNjVhYzJlOCIsInN1YiI6IjYxMmY2MTlkZmFjNTAyMDAyNmZkY2EyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WrL-NnkEwRCaGFDHqrPJARIvFRXR_Rq3uSxg6wnrQY0'
         }
 
-    });
-// const getGenres = async (url)=> await axiosMovies(urlGeners);
-const getMovie = async (id) => await axiosMovies('/movie/'+ id);
+    })
 export {axiosMovies};
