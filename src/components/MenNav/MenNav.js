76937d50c9bc8651b55908aa6f9813a61ca99178
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-09-17 23:16:38 +0200

    test react 2 break

--- src/components/MenNav/MenNav.js
+++ src/components/MenNav/MenNav.js
@@ -1,4 +1,5 @@
 import  React from 'react';
+import './MenNav.css';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
@@ -6,30 +7,22 @@ import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-
-// const  useStyles = makeStyles({
-//     root: {
-//         width: 500,
-//         position: "fixed",
-//         bottom: 0,
-//         backgroundColor: "#2d313a",
-//     },
-// });
 export default function SimpleBottomNavigation() {
     const [value, setValue] = React.useState(0);
 
     return (
-        <Box sx={{ width: 500 }}>
+        <Box sx={{ width: 500}}>
             <BottomNavigation
+                className={'root'}
                 showLabels
                 value={value}
                 onChange={(event, newValue) => {
                     setValue(newValue);
                 }}
             >
-                <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-                <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-                <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+                <BottomNavigationAction  label="Recents" icon={<RestoreIcon />} />
+                <BottomNavigationAction  label="Favorites" icon={<FavoriteIcon />} />
+                <BottomNavigationAction  label="Nearby" icon={<LocationOnIcon />} />
             </BottomNavigation>
         </Box>
     );
