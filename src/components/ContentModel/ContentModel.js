2fcbfadffb2f31731c6d2870a680192ba0948afa
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-09-19 22:47:06 +0200

    test react 2 break

--- src/components/ContentModel/ContentModel.js
+++ src/components/ContentModel/ContentModel.js
@@ -4,8 +4,15 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import {useState,useEffect} from "react";
+import {REACT_APP_API_KEY} from "../../constans/constans";
 import { makeStyles } from '@material-ui/styles';
+import axios from "axios";
+import {
+    img_500,
+    unavailable,
+    unavailableLandscape,
+} from "../../constans/constans";
 
 // const style = {
 //     position: 'absolute',
@@ -31,26 +38,64 @@ const useStyles = makeStyles((theme) => ({
         border: "1px solid #282c34",
         borderRadius: 10,
         color: "white",
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(1, 1, 3),
+        // boxShadow: theme.shadows[5],
+        // padding: theme.spacing(1, 1, 3),
     },
 }));
 
 
 
 export default function ContentModel({children, media_type, id}) {
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [content, setContent] = useState();
+    const [video, setVideo] = useState();
+    console.log(video);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const fetchData = async () => {
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${REACT_APP_API_KEY}&language=en-US`
+        );
+
+        setContent(data);
+
+    };
+
+    const fetchVideo = async () => {
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${REACT_APP_API_KEY}&language=en-US`
+        );
+
+        setVideo(data.results[0]?.key);
+    };
+
+    useEffect(() => {
+        fetchData();
+        fetchVideo();
+        // eslint-disable-next-line
+    }, []);
 
     return (
-        <div>
-            <Button type={'button'} className={'media'} onClick={handleOpen}>
+        <>
+            <div
+                className="media"
+                style={{ cursor: "pointer" }}
+                color="inherit"
+                onClick={handleOpen}
+            >
                 {children}
-            </Button>
+            </div>
             <Modal
                 aria-labelledby="transition-modal-title"
                 aria-describedby="transition-modal-description"
+                className={classes.modal}
                 open={open}
                 onClose={handleClose}
                 closeAfterTransition
@@ -60,16 +105,64 @@ export default function ContentModel({children, media_type, id}) {
                 }}
             >
                 <Fade in={open}>
-                    <Box sx={useStyles}>
-                        <Typography id="transition-modal-title" variant="h6" component="h2">
-                            Text in a modal
-                        </Typography>
-                        <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                        </Typography>
-                    </Box>
+                    {content && (
+                        <div className={classes.paper}>
+                            <div className="ContentModal">
+                                <img
+                                    src={
+                                        content.poster_path
+                                            ? `${img_500}/${content.poster_path}`
+                                            : unavailable
+                                    }
+                                    alt={content.name || content.title}
+                                    className="ContentModal__portrait"
+                                />
+                                <img
+                                    src={
+                                        content.backdrop_path
+                                            ? `${img_500}/${content.backdrop_path}`
+                                            : unavailableLandscape
+                                    }
+                                    alt={content.name || content.title}
+                                    className="ContentModal__landscape"
+                                />
+                                <div className="ContentModal__about">
+                  <span className="ContentModal__title">
+                    {content.name || content.title} (
+                      {(
+                          content.first_air_date ||
+                          content.release_date ||
+                          "-----"
+                      ).substring(0, 4)}
+                      )
+                  </span>
+                                    {content.tagline && (
+                                        <i className="tagline">{content.tagline}</i>
+                                    )}
+
+                                    <span className="ContentModal__description">
+                    {content.overview}
+                  </span>
+
+                                    <div>
+                                        {/*<Carousel id={id} media_type={media_type} />*/}
+                                    </div>
+
+                                    <Button
+                                        variant="contained"
+                                        // startIcon={<YouTubeIcon />}
+                                        color="secondary"
+                                        target="__blank"
+                                        href={`https://www.youtube.com/watch?v=${video}`}
+                                    >
+                                        Watch the Trailer
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </Fade>
             </Modal>
-        </div>
+        </>
     );
 }
